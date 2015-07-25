03b10e4555dcaa7efca52925b41173ba3eea15bc
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2015-07-25 21:26:49 +0100

    Update gsui.php
    Add `element` method

--- gsui.php
+++ gsui.php
@@ -5,5 +5,25 @@
 if (class_exists('GSUI')) return;
 
 class GSUI {
-  // ...
+  // == PUBLIC METHODS ==
+  public function element($tag, $attrs = array(), $content = null) {
+    $element = '<' . $tag . ' ';
+
+    foreach ($attrs as $attr => $val) {
+      $element .= $attr . '="' . $val . '"';
+    }
+
+    if (!$content) {
+      $element .= '/>';
+    } else {
+      if (is_array($content)) {
+        $content = implode("\n", $content);
+      } else {
+        $content = (string) $content;
+      }
+      $element .= '>' . $content . '</' . $tag . '>';
+    }
+
+    return $element;
+  }
 }
