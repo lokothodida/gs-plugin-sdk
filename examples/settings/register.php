40a6a67d67ff4df938571acc5a6221db9030ea1b
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2015-07-28 16:22:04 +0100

    Update register.php

---
+++ examples/settings/register.php
@@ -0,0 +1,25 @@
+<?php
+
+// TODO: Documentation
+include 'lib/gsplugin.php';
+include 'lib/gsutils.php';
+include 'lib/gsui.php';
+
+// Registration
+$plugin = new GSPlugin(array(
+  'id'       => $id,
+  'version'  => '1.0',
+  'author'   => 'You',
+  'website'  => 'http://yourwebsite.com',
+  'tab'      => 'theme',
+  'lang'     => 'en_US',
+));
+
+// Set the sidebar link
+$plugin->sidebar($plugin->i18n('PLUGIN_SIDEBAR'));
+
+// Setting the admin panel
+$plugin->admin('backend/index.php');
+
+// Initialization
+$plugin->init();
