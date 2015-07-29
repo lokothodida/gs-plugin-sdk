aba2638a5ed91ca67937e259ae007c59e565502a
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2015-07-29 13:25:54 +0100

    Add option for an sdk config

--- public/plugins/0_sdk/backend/build.php
+++ public/plugins/0_sdk/backend/build.php
@@ -4,15 +4,31 @@ try {
   unset($_POST['submitted']);
 
   foreach ($_POST as $name => $path) {
+    $basedir = GSPLUGINPATH . $name . '/';
+    $sdkconfig = $basedir . 'sdkconfig.json';
+
+    if (empty($path) && $utils->exists($sdkconfig)) {
+      $config = $utils->getfile($sdkconfig);
+
+      if (isset($config['buildpath'])) {
+        $path = $config['buildpath'];
+      }
+    }
+
     if (!empty($path)) {
-      $basedir = GSPLUGINPATH . $name . '/';
       $dest = GSROOTPATH . '/build/' . $path . '/';
       $file = $name . '.php';
 
       $utils->copy(GSPLUGINPATH . $file, $dest . $file);
       $utils->copy($basedir, $dest . $name);
+
       // Copy the SDK
-      //$utils->dump($basedir, $dest);
+      $sdk = array('gsutils', 'gsplugin', 'gsui');
+      foreach ($sdk as $item) {
+        $file = '/lib/' . $item . '.php';
+        $utils->copy($plugin->path() . $file, $dest . $name . $file);
+      }
+
       echo $ui->success($plugin->i18n('BUILD_SUCC'));
     }
   }
