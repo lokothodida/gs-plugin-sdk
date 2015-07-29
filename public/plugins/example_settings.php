deb05956ccb1b5c1d04d8df970b1454b7a1d1353
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2015-07-29 15:36:48 +0100

    Fix initialization

--- public/plugins/example_settings.php
+++ public/plugins/example_settings.php
@@ -1,17 +1,17 @@
 <?php
 
-// This file @includes the /register.php script in your plugin's folder
-// but protects the scope of the code in that file by wrapping the execution
-// in an anonymous, immediately invoked function(*).
-// It also uses the name of this file as the @id for the rest of the plugin
-// and imports that variable into the scope of the /register.php script.
 call_user_func(create_function('$id', '
-  include($id . "/register.php");
-'), basename(__FILE__, ".php"));
+  // Load SDK library
+  $libs = array("plugin", "utils", "ui");
 
-// (*) @create_function is being used instead of a Closure because GetSimple
-//     supports PHP 5.2+, and Closures were only introduced in 5.3. Ideally
-//     the code would look like this (PHP 5.3+):
-//     call_user_func(function($id) {
-//       include $id . '/register.php';
-//     }, basename(__FILE__, '.php'));
+  foreach ($libs as $lib) {
+    $file = GSPLUGINPATH . $id . "/lib/gs" . $lib . ".php";
+
+    if (file_exists($file)) {
+      include $file;
+    }
+  }
+
+  // Run plugin
+  include $id . "/index.php";
+'), basename(__FILE__, ".php"));
